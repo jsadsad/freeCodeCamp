00c65d812b73d176e42edaf5addbd2f214c7e1ec
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-03-14 08:18:38 -0700

    add recursion and ternary

---
+++ basic_js_ternary.js
@@ -0,0 +1,7 @@
+// Basic JavaScript: Use Multiple Conditional (Ternary) OperatorsPassed
+
+function checkSign(num) {
+    return num > 0 ? "positive" : (num === 0) ? "zero" : "negative"
+}
+
+checkSign(10);
