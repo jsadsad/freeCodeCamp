4cbbfeee65c1183c0eef82bad5644433ce02fcc9
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-03-23 19:54:21 -0700

    destructuring assignment basics

---
+++ es6_use_deconstructing_assignments_to_extract_values_from objects.js
@@ -0,0 +1,14 @@
+// Destructuring assignment is special syntax introduced in ES6, for neatly assigning values taken directly from an object.
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES
+
+// Only change code above this line
+// today should be equal to 77 and tomorrow should be equal to 80.
