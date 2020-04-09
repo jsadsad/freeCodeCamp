ea767c3f7b1599892980ba917e8ac2b0b143f7e3
Joshua Clarence D. Sadsad <joshsad2@gmail.com>
2020-04-08 22:37:49 -0700

    resolving a promise

---
+++ es6_completing_a_promise.js
@@ -0,0 +1,12 @@
+// A promise has three states: pending, fulfilled, and rejected. The promise you created in the last challenge is forever stuck in the pending state because you did not add a way to complete the promise. The resolve and reject parameters given to the promise argument are used to do this. resolve is used when you want your promise to succeed, and reject is used when you want it to fail. These are methods that take an argument, as seen below.
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if(responseFromServer) {
+    resolve("We got the data")
+  } else {
+    reject("Data not received")
+  }
+});
